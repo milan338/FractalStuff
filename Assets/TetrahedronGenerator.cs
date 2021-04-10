16b4e8f4e53fe4d654f5e062227ee143dc4ae2bd
milan338 <56327606+milan338@users.noreply.github.com>
2021-04-10 23:00:16 +0930

    Small cleanup

--- Assets/TetrahedronGenerator.cs
+++ Assets/TetrahedronGenerator.cs
@@ -17,39 +17,36 @@ public class TetrahedronGenerator : MonoBehaviour
     private int? max_iterations = null;
     private int? current_iteration = null;
 
-    // Update private variables
-    public void SetData(float x, float y, float z, float a, int n, int i)
-    {
-        start_x = x;
-        start_y = y;
-        start_z = z;
-        base_length = a;
-        max_iterations = n;
-        current_iteration = i;
-    }
-
     // Run when object is created
     private void Start()
     {
-        Debug.Log(base_length + " " + max_iterations + " " + current_iteration);
+        // Get start coordinates
         float x = start_x.HasValue ? start_x.Value : 0;
         float y = start_y.HasValue ? start_y.Value : 0;
         float z = start_z.HasValue ? start_z.Value : 0;
+        // Get original tetrahedron length
         float a = base_length.HasValue ? base_length.Value : 50;
+        // Get total number of iterations to run
         int n = max_iterations.HasValue ? max_iterations.Value : 5;
+        // Get the current iteration of the object
         int i = current_iteration.HasValue ? current_iteration.Value : 1;
+        // Draw remainder of fractal
+        DrawTetrahedronFractal(x, y, z, a, n, i);
+    }
+
+    // Run on frame update
+    private void Update() { }
+
+    // Update object with new mesh and material
+    private void CreateMesh()
+    {
         // Add mesh to object
         mesh = new Mesh();
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
         // Add diffuse material to object
         material = new Material(Shader.Find("Diffuse"));
         gameObject.AddComponent<MeshRenderer>().material = material;
-        // Draw additional tetrahedra
-        DrawTetrahedronFractal(x, y, z, a, n, i);
     }
-
-    // Run on frame update
-    private void Update() { }
 
     // Draw tetrahedra or continue recursion
     private void DrawTetrahedronFractal(float x, float y, float z, float a, int n, int i)
@@ -62,9 +59,11 @@ public class TetrahedronGenerator : MonoBehaviour
         // Draw tetrahedron at bottom iteration
         else if (n == i)
             DrawTetrahedron(x, y, z, l);
+        // Draw mesh from calculated points
         if (n == 1 | n == i)
         {
             // Update mesh
+            CreateMesh();
             mesh.Clear();
             mesh.vertices = vertices;
             mesh.triangles = triangles;
@@ -91,15 +90,17 @@ public class TetrahedronGenerator : MonoBehaviour
                 .AddComponent<TetrahedronGenerator>()
                 .SetData(points[j].x, points[j].y, points[j].z, a, n, i + 1);
             }
-            // Destroy current gameobject
-            Destroy(gameObject);
+            // Don't destroy root object
+            if (i != 1)
+                // Destroy current game object
+                Destroy(gameObject);
         }
     }
 
     // Draw single tetrahedron
     private void DrawTetrahedron(float x, float y, float z, float l)
     {
-        Debug.Log(x + " " + y + " " + z + " " + l);
+        // Vertices for individual tetrahedron
         vertices = new Vector3[]
         {
             // Triangle 0 - bottom vertex
@@ -111,6 +112,7 @@ public class TetrahedronGenerator : MonoBehaviour
             // Triangle 3 - top vertex
             new Vector3(x + (l / 2f), y + (l * (Mathf.Sqrt(6f) / 3f)), z + ((l / 2f) * Mathf.Tan(Mathf.PI / 6f)))
         };
+        // Order to create triangles from vertices in
         triangles = new int[]
         {
             0,1,2,
@@ -118,5 +120,16 @@ public class TetrahedronGenerator : MonoBehaviour
             2,1,3,
             0,3,1
         };
+    }
+
+    // Update private variables
+    public void SetData(float x, float y, float z, float a, int n, int i)
+    {
+        start_x = x;
+        start_y = y;
+        start_z = z;
+        base_length = a;
+        max_iterations = n;
+        current_iteration = i;
     }
 }
